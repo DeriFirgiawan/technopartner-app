d68e2cd3185cdb5d3b9bdcf025af3c482b516d7a
derifirgiawan_025 <derifirgiawan025@gmail.com>
2021-08-18 01:34:16 +0700

    modified

--- src/Components/MenuComponent/SectionHeader/index.js
+++ src/Components/MenuComponent/SectionHeader/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { HashLink as Link } from 'react-router-hash-link';
 import RootNavbar from "../../RootNavbar";
 
 const SectionHeader = ({data}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const handleClick = index => {
+    console.log(index);
+    setActiveIndex(index);
+  }
   return (
     <header className="menu-header fixed-top">
       <h3>Menu</h3>
@@ -12,8 +17,8 @@ const SectionHeader = ({data}) => {
             {
               data.map((value, index) => {
                 return (
-                  <div className="menu-link-item">
-                    <Link  to={`/menu#${index}`} className="menu-link" key={index}>{value.category_name}</Link>
+                  <div className={`menu-link-item ${activeIndex === index ? 'active' : ''}`}>
+                    <Link onClick={() => handleClick(index)} to={`/menu#${index}`} className="menu-link" key={index}>{value.category_name}</Link>
                   </div>
                 )
               })
