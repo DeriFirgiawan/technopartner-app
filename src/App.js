b48079e8422cead140f82975f6e1222c66eb4665
derifirgiawan_025 <derifirgiawan025@gmail.com>
2021-08-17 16:20:10 +0700

    slicing login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import Routers from './Routes';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="main-page">
+      <Routers />
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Page/Menu/index.js
@@ -0,0 +1,7 @@
+const MenuPage = () => {
+  return (
+    <h1>Home Page</h1>
+  );
+};
+
+export default MenuPage;
