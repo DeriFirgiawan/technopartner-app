6e8f56206ecf853da885efee4828dd1bb05424ec
derifirgiawan_025 <derifirgiawan025@gmail.com>
2021-08-18 01:18:02 +0700

    modified slider

--- src/Components/HomeComponent/SectionCarousel/index.js
+++ src/Components/HomeComponent/SectionCarousel/index.js
@@ -1,26 +1,53 @@
 import React from "react";
 import { Icon } from '@iconify/react';
-import RBCarousel from 'react-bootstrap-carousel';
+import ReactBootstrapCarousel  from 'react-bootstrap-carousel';
 
 const SectionCarousel = ({data}) => {
   return (
-    <RBCarousel
-      animation={true}
-      autoPlay={true}
-      slideshowSpeed={2000}
-      defaultActiveIndex={0}
-      leftIcon={false}
-      rightIcon={false}
-      version={4}
-    >
-      {
-        data.map((value, key) => {
-          return (
-            <img src={value} key={key} className="d-block w-100" alt="TechPartner" />
-          )
-        })
-      }
-    </RBCarousel>
+    <React.Fragment>
+      <ReactBootstrapCarousel 
+        animation={true}
+        autoPlay={true}
+        slideshowSpeed={2000}
+        defaultActiveIndex={0}
+        leftIcon={<span></span>}
+        rightIcon={<span></span>}
+        version={4}
+      >
+        {
+          data.map((value, key) => {
+            return (
+              <React.Fragment>
+                <img src={value} key={key} className="d-block w-100" alt="TechPartner" />
+              </React.Fragment>
+            )
+          })
+        }
+      </ReactBootstrapCarousel>
+      <div className="more-content">
+          <h6>View all</h6>
+        <div>
+          <Icon icon="entypo:chevron-small-right" fontSize={24} />
+        </div>
+      </div>
+      {/* <div className="wrapper-indicator">
+        <div className="carousel-indicators">
+          {
+            data.map((value, index) => {
+              return (
+                <span className="dotted" key={index} />
+              )
+            })
+          }
+        </div>
+        <div className="more-content">
+            <h6>View all</h6>
+          <div>
+            <Icon icon="entypo:chevron-small-right" fontSize={24} />
+          </div>
+        </div>
+      </div> */}
+    </React.Fragment>
     // <React.Fragment>
     //   <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" data-ride="carousel" data-interval={2000}>
     //     <div className="carousel-inner">
